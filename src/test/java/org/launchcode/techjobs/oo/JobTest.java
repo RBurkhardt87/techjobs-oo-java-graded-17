dc5a60301efb9f139ead49356a2f2902cfaed4db
Rachel Burkhardt <r.burkhardt.ibclc@gmail.com>
2024-10-16 18:06:43 -0500

    Does it matter that I use assertTrue?

--- src/test/java/org/launchcode/techjobs/oo/JobTest.java
+++ src/test/java/org/launchcode/techjobs/oo/JobTest.java
@@ -76,8 +76,8 @@ public class JobTest {
 //       String expected = System.lineSeparator() + System.lineSeparator();
 //       assertEquals(expected, actual);     <--- I DO NOT UNDERSTAND HOW TO DO THIS AS AN EQUAL. WHEN I START BUILDING MORE FOR toString, TEST FAILS BECAUSE THEY AREN'T EQUAL ANYMORE
 
-        assertTrue(actual1.startsWith(System.lineSeparator()));
-        assertTrue(actual2.endsWith(System.lineSeparator()));
+        assertTrue(job6.toString().startsWith(System.lineSeparator()));
+        assertTrue(job6.toString().endsWith(System.lineSeparator()));
         //What about this... why wouldn't this work. Testing true for true with assertEquals()
 //        assertEquals(true, actual1);
 //        assertEquals(true, actual2);
